7556c7c2fabeec00a8daf435d42b81ed7b8c3e79
Partho Debnath <parthodebnath28@gmail.com>
2023-05-30 23:58:07 +0600

    change app name in admin panel

--- djangoDRFbackend/bookapi/apps.py
+++ djangoDRFbackend/bookapi/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class BookapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bookapi'
+    verbose_name = "Book API"    # change app name in admin panel
